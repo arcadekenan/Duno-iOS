ac302eb45cb0a8d1668b2735d27acf7568007f5a
Davi Bispo <davibispo568@gmail.com>
2020-10-19 14:29:44 -0300

    iOS App startup

---
+++ Duno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Duno
+//
+//  Created by Davi Bispo on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
